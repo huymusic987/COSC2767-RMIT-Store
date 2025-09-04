0b24d87733fa4a8d9eca0c7e57ed310f835435ac
huymusic987 <huymarky05@gmail.com>
2025-09-04 10:22:19 +0700

    yes

--- server/index.js
+++ server/index.js
@@ -50,7 +50,7 @@ const server = app.listen(port, "0.0.0.0", () => {
       `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
     )}`
   );
-  console.log(`${chalk.green("huymusic-2")}`);
+  console.log(`${chalk.green("huymusic-3")}`);
 });
 
 // Initialize WebSocket server
